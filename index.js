89b9b9fb4412147055044ee3d6abf90a28c32f45
Jacksen Nillson <nillsonjacksen@gmail.com>
2023-08-19 16:12:55 +0800

    trial of different event methods on mobile for card flip / rotation

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
  // JavaScript to toggle card rotation on click
  const cards = document.querySelectorAll(".card");
-    
+
  cards.forEach(card => {
    card.addEventListener("click", function() {
+     card.classList.toggle("is-flipped");
+   });
+ 
+   card.addEventListener("touchstart", function() {
      card.classList.toggle("is-flipped");
    });
  });
